0e5088bae4b25a3e40a7bbf1e52692c0129587c7
JRC <jrcelebrados@gmail.com>
2020-12-18 04:18:37 -0500

    Created book component

---
+++ src/components/books/Bible.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Bible extends Component.React {
+
+  render() {
+
+    return(
+      <div class="container">
+
+        <div>Hi</div>
+
+      </div>
+
+    );
+
+  }
+
+}
+
+export default Bible;
---
+++ src/components/books/BookExample.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function BookExample() {
+  return (
+    <h1>Hello</h1>
+  );
+}
+
+export default BookExample;
